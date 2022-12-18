4d3339e325982be931568013af18b8c603afa806
Wigdo <51504031+Wigdo@users.noreply.github.com>
2022-12-19 00:06:15 +1100

    Everything is being saved to index 0 of the array; not iterating properly...

--- Wigdo/TestTwo.java
+++ Wigdo/TestTwo.java
@@ -4,32 +4,43 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.util.ArrayList;
+
 public class TestTwo
 {
-
-    public static void main (String [] args)
-    {
-        final String url = "https://careers.pageuppeople.com/736/cw/en/filter/?search-keyword=&location=open%20australia%20wide&location=victoria&job-mail-subscribe-privacy=agree";
-
-        try{
-            final Document docObj = Jsoup.connect(url).get();
-
-            for (Element row : docObj.select("tbody#search-results-content"))
-            {
-                    System.out.println(row.select(".job-link").text());
-            }
-
-            for (Element row : docObj.select("tbody#search-results-content"))
-            {
-                System.out.println(row.select(".location").text());
-            }
+        private ArrayList<String> arr1;
 
 
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+    public TestTwo() {
+        arr1 = new ArrayList<>();
+        MauriceBlackburn();
     }
+
+
+
+        public void MauriceBlackburn () {
+
+            final String url = "https://careers.pageuppeople.com/736/cw/en/filter/?search-keyword=&location=open%20australia%20wide&location=victoria&job-mail-subscribe-privacy=agree";
+
+            try {
+                final Document docObj = Jsoup.connect(url).get();
+
+                for (Element row : docObj.select("tbody#search-results-content")) {
+
+                    System.out.println(row.select(".job-link").text());
+                }
+
+                for (Element row : docObj.select("tbody#search-results-content")) {
+                    System.out.println(row.select(".location").text());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    public static void main (String [] args)
+        {
+        TestTwo newTest = new TestTwo();
+        }
 
 }
