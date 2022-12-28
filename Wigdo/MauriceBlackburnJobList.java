155b49c2a883849e03ac3684478c06b431a0f7b1
Wigdo <51504031+Wigdo@users.noreply.github.com>
2022-12-29 09:52:11 +1100

    Added list of jobs (and in a format able to be arrayed) for Maurice Blackburn Job list.

--- Wigdo/MauriceBlackburnJobList.java
+++ Wigdo/MauriceBlackburnJobList.java
@@ -9,13 +9,15 @@ import java.util.ArrayList;
 
 public class MauriceBlackburnJobList
 {
-    private ArrayList<String> arr1;
+    private ArrayList<String> jobArray;
+    private ArrayList<String> locationArray;
 
     public MauriceBlackburnJobList() {
-        arr1 = new ArrayList<>();
-        getJobList();
+        jobArray = new ArrayList<>();
+        locationArray = new ArrayList<>();
+        collateJobList();
     }
-    public void getJobList () {
+    public void collateJobList () {
         final String url = "https://careers.pageuppeople.com/736/cw/en/filter/?search-keyword=&job-mail-subscribe-privacy=agree";
         try {
             final Document docObj = Jsoup.connect(url).get();
@@ -26,15 +28,59 @@ public class MauriceBlackburnJobList
 
                 if (row.parent().attributes().hashCode() == 1679553498)
                 {
-                    System.out.println(row.select(".job-link").text());
+                    jobArray.add(row.select(".job-link").text());
+                    locationArray.add(row.select(".location").text());
                 }
-
             }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void displayJobs ()
+    {
+        int i=0;
+        for (i=0; i < jobArray.size(); i++)
+        {
+            System.out.println(jobArray.get(i));
+        }
+    }
+
+    public void displayJobLocations ()
+    {
+        int i =0;
+        for (i=0; i < locationArray.size(); i++)
+        {
+            System.out.println(locationArray.get(i));
+        }
+    }
+
+    public String getJobAtIndex (int index)
+    {
+        if (jobArray.size() >= index)
+        {
+            return jobArray.get(index);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public String getLocationAtIndex (int index)
+    {
+        if (locationArray.size() >= index)
+        {
+            return locationArray.get(index);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
     public static void main (String [] args)
     {
         MauriceBlackburnJobList newTest = new MauriceBlackburnJobList();
